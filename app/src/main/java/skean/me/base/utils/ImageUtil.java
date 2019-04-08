bfe4c6b17d89102a823cfd813649c607c3815871
Skean <70709695@qq.com>
2019-04-08 17:32:52 +0800

    修改BUG

--- app/src/main/java/skean/me/base/utils/ImageUtil.java
+++ app/src/main/java/skean/me/base/utils/ImageUtil.java
@@ -16,6 +16,7 @@ import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -372,123 +373,143 @@ public class ImageUtil {
             }
 
         }
+    }
+
+    /**
+     * Base64操作类
+     */
+    public static class Base64Util {
 
         /**
-         * Base64操作类
+         * 将图片转换为Base64编码
+         *
+         * @param image 图片的file
+         * @return 转换后的字符串
          */
-        public static class Base64Util {
+        public static String encodeToString(File image) {
+            InputStream is;
+            byte[] data;
+            //读取图片字节数组
+            try {
+                is = new FileInputStream(image);
+                data = new byte[is.available()];
+                is.read(data);
+                is.close();
+                return Base64.encodeToString(data, Base64.DEFAULT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-            /**
-             * 将图片转换为Base64编码
-             *
-             * @param image 图片的file
-             * @return 转换后的字符串
-             */
-            public static String encodeToString(File image) {
-                InputStream is;
-                byte[] data;
-                //读取图片字节数组
-                try {
-                    is = new FileInputStream(image);
-                    data = new byte[is.available()];
-                    is.read(data);
-                    is.close();
-                    return Base64.encodeToString(data, Base64.DEFAULT);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        /**
+         * 将图片转换为Base64编码
+         *
+         * @param bitmap 图片的file
+         * @return 转换后的字符串
+         */
+        public static String encodeToString(Bitmap bitmap) {
+            //读取图片字节数组
+            try {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
+                byte[] byteArray = bos.toByteArray();
+                return Base64.encodeToString(byteArray, Base64.DEFAULT);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        /**
+         * 图片转换为Base64编码字节数组
+         *
+         * @param image 图片的file
+         * @return 转换后的字节数组
+         */
+        public static byte[] encodeToBytes(File image) {
+            InputStream is;
+            byte[] data;
+            byte[] result = null;
+            //读取图片字节数组
+            try {
+                is = new FileInputStream(image);
+                data = new byte[is.available()];
+                is.read(data);
+                is.close();
+                result = Base64.encode(data, Base64.DEFAULT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        /**
+         * 将base64字符串转化成图片Bitmap
+         *
+         * @param base64Str base64编码字符串
+         * @return Bitmap数据
+         */
+        public static Bitmap decodeAsBitmap(String base64Str) {   //对字节数组字符串进行Base64解码并生成图片
+            if (base64Str == null) //图像数据为空
                 return null;
-            }
-
-            /**
-             * 图片转换为Base64编码字节数组
-             *
-             * @param image 图片的file
-             * @return 转换后的字节数组
-             */
-            public static byte[] encodeToBytes(File image) {
-                InputStream is;
-                byte[] data;
-                byte[] result = null;
-                //读取图片字节数组
-                try {
-                    is = new FileInputStream(image);
-                    data = new byte[is.available()];
-                    is.read(data);
-                    is.close();
-                    result = Base64.encode(data, Base64.DEFAULT);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return result;
-            }
-
-            /**
-             * 将base64字符串转化成图片Bitmap
-             *
-             * @param base64Str base64编码字符串
-             * @return Bitmap数据
-             */
-            public static Bitmap decodeAsBitmap(String base64Str) {   //对字节数组字符串进行Base64解码并生成图片
-                if (base64Str == null) //图像数据为空
-                    return null;
-                try {
-                    //Base64解码
-                    byte[] data = Base64.decode(base64Str.getBytes(), Base64.DEFAULT);
-                    for (int i = 0; i < data.length; ++i) {
-                        if (data[i] < 0) {//调整异常数据
-                            data[i] += 256;
-                        }
+            try {
+                //Base64解码
+                byte[] data = Base64.decode(base64Str.getBytes(), Base64.DEFAULT);
+                for (int i = 0; i < data.length; ++i) {
+                    if (data[i] < 0) {//调整异常数据
+                        data[i] += 256;
                     }
-                    return BitmapFactory.decodeByteArray(data, 0, data.length);
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
-                return null;
+                return BitmapFactory.decodeByteArray(data, 0, data.length);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            return null;
+        }
 
-            /**
-             * 将base64字符串转化成图片数据字节数组
-             *
-             * @param base64Str base64编码字符串
-             * @return Bitmap数据
-             */
-            public static byte[] decodeAsBytes(String base64Str) {   //对字节数组字符串进行Base64解码并生成图片
-                if (base64Str == null) //图像数据为空
-                    return null;
-                try {
-                    //Base64解码
-                    byte[] data = Base64.decode(base64Str.getBytes(), Base64.DEFAULT);
-                    for (int i = 0; i < data.length; ++i) {
-                        if (data[i] < 0) {//调整异常数据
-                            data[i] += 256;
-                        }
-                    }
-                    return data;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        /**
+         * 将base64字符串转化成图片数据字节数组
+         *
+         * @param base64Str base64编码字符串
+         * @return Bitmap数据
+         */
+        public static byte[] decodeAsBytes(String base64Str) {   //对字节数组字符串进行Base64解码并生成图片
+            if (base64Str == null) //图像数据为空
                 return null;
+            try {
+                //Base64解码
+                byte[] data = Base64.decode(base64Str.getBytes(), Base64.DEFAULT);
+                for (int i = 0; i < data.length; ++i) {
+                    if (data[i] < 0) {//调整异常数据
+                        data[i] += 256;
+                    }
+                }
+                return data;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            return null;
+        }
 
-            //base64字符串转化成图片
-            public static Bitmap decodeAsBitmap(byte[] rawData) {   //对字节数组字符串进行Base64解码并生成图片
-                if (rawData == null) //图像数据为空
-                    return null;
-                try {
-                    //Base64解码
-                    byte[] data = Base64.decode(rawData, Base64.DEFAULT);
-                    for (int i = 0; i < data.length; ++i) {
-                        if (data[i] < 0) {//调整异常数据
-                            data[i] += 256;
-                        }
-                    }
-                    return BitmapFactory.decodeByteArray(data, 0, data.length);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        //base64字符串转化成图片
+        public static Bitmap decodeAsBitmap(byte[] rawData) {   //对字节数组字符串进行Base64解码并生成图片
+            if (rawData == null) //图像数据为空
                 return null;
+            try {
+                //Base64解码
+                byte[] data = Base64.decode(rawData, Base64.DEFAULT);
+                for (int i = 0; i < data.length; ++i) {
+                    if (data[i] < 0) {//调整异常数据
+                        data[i] += 256;
+                    }
+                }
+                return BitmapFactory.decodeByteArray(data, 0, data.length);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            return null;
         }
     }
 }
