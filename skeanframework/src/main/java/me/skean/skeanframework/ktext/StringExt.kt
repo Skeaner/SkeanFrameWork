c9febf3a84869c0b184ea2d807f4e233e032f33b
skean <70709695@qq.com>
2021-12-01 17:37:34 +0800

    添加弹出快捷方式

--- skeanframework/src/main/java/me/skean/skeanframework/ktext/StringExt.kt
+++ skeanframework/src/main/java/me/skean/skeanframework/ktext/StringExt.kt
@@ -1,5 +1,7 @@
 package me.skean.skeanframework.ktext
 
+import com.blankj.utilcode.util.ToastUtils
+
 /**
  * Created by Skean on 21/6/11.
  */
@@ -8,3 +10,15 @@ fun String?.orDash(): String = this ?: "-"
 fun String?.orBlank(): String = this ?: " "
 
 fun String?.or(defaultValue: String): String = this ?: defaultValue
+
+fun String?.showAsShortToast() {
+    if (this != null) {
+        ToastUtils.showShort(this)
+    }
+}
+
+fun String?.showAsLongToast() {
+    if (this != null) {
+        ToastUtils.showLong(this)
+    }
+}
