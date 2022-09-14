52810c83962e0abef9ebaabe09eaeb630afcfc69
skean <70709695@qq.com>
2022-09-14 17:35:36 +0800

    更新地图, 修复一些BUG

---
+++ skeanframework/src/main/java/me/skean/skeanframework/rx/RetryWithDelay.kt
@@ -0,0 +1,66 @@
+package me.skean.skeanframework.rx
+
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.functions.Function
+import org.reactivestreams.Publisher
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * 自动重试
+ * @param maxRetries Int  最大重试次数, 0为无限重试
+ * @param retryDelayMillis Int 每次重试的延迟毫秒数
+ * @return Observable<T>
+ */
+fun <T> Observable<T>.retryWhenWithTimesAndDelay(maxRetries: Int, retryDelayMillis: Int): Observable<T> {
+    return this.retryWhen(RetryWithDelay(maxRetries, retryDelayMillis))
+}
+
+/**
+ *
+ * @param maxRetries Int 最大重试次数, 0为无限重试
+ * @param retryDelayMillis Int Int 每次重试的延迟毫秒数
+ * @return Single<T>
+ */
+fun <T> Single<T>.retryWhenWithTimesAndDelay(maxRetries: Int, retryDelayMillis: Int): Single<T> {
+    return this.retryWhen(RetryWithDelaySingle(maxRetries, retryDelayMillis))
+}
+
+class RetryWithDelay(private val maxRetries: Int, private val retryDelayMillis: Int) :
+    Function<Observable<out Throwable?>, Observable<*>> {
+    private var retryCount = 0
+    override fun apply(attempts: Observable<out Throwable?>): Observable<*> {
+        return attempts
+            .flatMap { t ->
+                if (maxRetries == 0 || ++retryCount < maxRetries) {
+                    // When this Observable calls onNext, the original
+                    // Observable will be retried (i.e. re-subscribed).
+                    Observable.timer(
+                        retryDelayMillis.toLong(),
+                        TimeUnit.MILLISECONDS
+                    )
+                } else Observable.error<Any>(t)
+            }
+    }
+}
+
+class RetryWithDelaySingle(private val maxRetries: Int, private val retryDelayMillis: Int) :
+    Function<Flowable<out Throwable?>, Flowable<*>> {
+    private var retryCount = 0
+
+    override fun apply(attempts: Flowable<out Throwable?>): Flowable<*> {
+        return attempts
+            .flatMap { t ->
+                if (maxRetries == 0 || ++retryCount < maxRetries) {
+                    // When this Observable calls onNext, the original
+                    // Observable will be retried (i.e. re-subscribed).
+                    Flowable.timer(
+                        retryDelayMillis.toLong(),
+                        TimeUnit.MILLISECONDS
+                    )
+                } else Flowable.error<Any>(t)
+            }
+    }
+}
